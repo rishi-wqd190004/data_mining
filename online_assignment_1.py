0b4a63c8e51c927840d351265af645718dfdf0ea
Rishi <55717162+rishi-wqd190004@users.noreply.github.com>
2020-05-08 18:46:01 +0800

    online assignment week 7
    this data is scraped from a website and then cleared into a dataframe

---
+++ online_assignment_1.py
@@ -0,0 +1,45 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+url = "https://coinmarketcap.com/currencies/bitcoin/historical-data/"
+html = urlopen(url).read()
+soap_html = soup(html, "html.parser")
+div = soap_html.findAll("div", {"class": "cmc-table__table-wrapper-outer"})
+
+#table = div[2].findAll("table")
+#print(table)
+#thead = div[2].findAll("thead")
+#th = thead[0].findAll("th")
+#th_list =[]
+#for element in th:
+#	th_list.append(element.text)
+#print(th_list)
+tbody = div[2].findAll("tr")
+header = tbody[0].findAll("th")
+header_list = []
+for h in header:
+	header_list.append(h.text)
+
+#print(header_list)
+tbody2 = tbody[1:]
+#print(tbody2)
+content = []
+for element in tbody2:
+	t = []
+	td = element.findAll("td")
+	for ele in td:
+		t.append(ele.text)
+	content.append(t)
+
+#print(content)
+df = pd.DataFrame(content,columns=header_list)
+df[["Date"]] = df[["Date"]].apply(pd.to_datetime,errors='ignore')
+df[["Open*", "High", "Low", "Close**"]] = df[["Open*", "High", "Low", "Close**"]].apply(lambda x: x.str.replace(',', '').astype(float), axis=1)
+df[["Volume", "Market Cap"]] = df[["Volume", "Market Cap"]].apply(lambda a: a.str.replace(',', '').astype(int), axis=1)
+#df[["Open*"]] = df[["Open*"]].apply(pd.to_numeric, downcast='int')
+#print(df.info())
+#df.drop(df.columns[[1]], axis=1)
+print(df)
+
+df.to_csv("~/masters_semester_2/data_mining_WQD7005/tests_script/bitcoin_price.csv")
+#print(tbody)
